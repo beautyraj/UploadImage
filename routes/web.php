d5cc8dc43e49978c00f82b7f869ffaad63c4e016
Beauty Raj <beautyraj@virtualemployee.com>
2022-07-29 13:33:22 +0530

    worked on routes

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//download image
 Route::get("downloadFile/{id}","App\Http\Controllers\UploadController@downloadFile");
